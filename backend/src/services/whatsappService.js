2c6e03e08e8cd157e1a51323e1e8f9e8e7ab0ce4
boga-venu-dev <venugopalboga6@gmail.com>
2024-10-11 08:48:07 +0530

    Update whatsappService.js

--- backend/src/services/whatsappService.js
+++ backend/src/services/whatsappService.js
@@ -2,18 +2,20 @@ const axios = require('axios');
 const config = require('../config');
 const logger = require('../utils/logger');
 const Message = require('../models/message');
-
-const WHATSAPP_API_URL = `https://graph.facebook.com/${config.whatsapp.apiVersion}/${config.whatsapp.phoneNumberId}/messages`;
+const Settings = require('../models/Settings');
 
 async function sendMessage(to, templateName, templateData) {
   try {
+    const settings = await Settings.findOne();
+    const WHATSAPP_API_URL = `https://graph.facebook.com/${config.whatsapp.apiVersion}/${settings.whatsappPhoneNumber}/messages`;
+
     const response = await axios.post(WHATSAPP_API_URL, {
       messaging_product: "whatsapp",
       to: to,
       type: "template",
       template: { 
         name: templateName, 
-        language: { code: "en_US" },
+        language: { code: settings.defaultLanguage },
         components: templateData
       }
     }, {
@@ -41,14 +43,10 @@ async function sendMessage(to, templateName, templateData) {
 
 async function getMessageTemplates() {
   try {
-    const response = await axios.get(`https://graph.facebook.com/${config.whatsapp.apiVersion}/${config.whatsapp.businessAccountId}/message_templates`, {
-      headers: {
-        'Authorization': `Bearer ${config.whatsapp.accessToken}`,
-      }
-    });
-    return response.data.data;
+    const settings = await Settings.findOne();
+    return settings.messageTemplates;
   } catch (error) {
-    logger.error('Error fetching message templates:', error.response ? error.response.data : error.message);
+    logger.error('Error fetching message templates:', error);
     throw error;
   }
 }
