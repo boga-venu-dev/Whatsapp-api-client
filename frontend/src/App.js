70e1cbd8f81145bf833f67e30e3a27e6be4bb534
boga-venu-dev <venugopalboga6@gmail.com>
2024-10-11 08:39:37 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Messages from './pages/Messages';
 import Login from './pages/Login';
+import Settings from './pages/Settings';
 import './styles/App.css';
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/messages">Messages</Link></li>
-            <li><Link to="/login">Login</Link></li>
-          </ul>
-        </nav>
+      <div className="app">
+        <header className="app-header">
+          <nav>
+            <ul className="app-nav">
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/messages">Messages</Link></li>
+              <li><Link to="/settings">Settings</Link></li>
+              <li><Link to="/login">Login</Link></li>
+            </ul>
+          </nav>
+        </header>
 
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/messages" component={Messages} />
-          <Route path="/login" component={Login} />
-        </Switch>
+        <main className="app-content">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/messages" component={Messages} />
+            <Route path="/settings" component={Settings} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </main>
       </div>
     </Router>
   );
